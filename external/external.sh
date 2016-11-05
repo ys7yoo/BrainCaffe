da3fb0fdea2664041bdad3b999576780185956cc
YY <ys7yoo@gmail.com>
2016-11-05 09:52:33 +0900

    fix folder names

--- external/external.sh
+++ external/external.sh
@@ -3,9 +3,17 @@
 #wget https://github.com/google/glog/archive/master.zip
 wget https://github.com/google/glog/archive/v0.3.4.zip
 
-unzip master 
+#unzip master 
+unzip v0.3.4.zip
 
-cd glog-master
+#cd glog-master
+cd glog-0.3.4
 
-./configure --enable-shared=no --enable-static=yes --prefix=/Users/yyoo/src_quantum/caffe/external/glog-install
+
+
+CURRENT=`pwd`
+
+# folder changed
+# ./configure --enable-shared=no --enable-static=yes --prefix=/Users/yyoo/src/caffe-rc3/external/glog-install
+./configure --enable-shared=no --enable-static=yes --prefix=$CURRENT/glog-install
 
