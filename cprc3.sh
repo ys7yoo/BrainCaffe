6b851ed823ed08a7c42b862c84ff7bd1bb44b7ff
YY <ys7yoo@gmail.com>
2016-11-05 09:39:49 +0900

    added a simple script to copy files to ../caffe-rc3

---
+++ cprc3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# simple script to setup with RC3
+ 
+
+# copy Makefile and Makefile.config to ../caffe-rc3
+cp Makefile ../caffe-rc3/
+cp Makefile.config ../caffe-rc3/
+
+cp -r external ../caffe-rc3/
+
