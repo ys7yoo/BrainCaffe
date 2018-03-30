328316c192726d5da41f41c1ce4c8affd0f106d4
YY <ys7yoo@gmail.com>
2018-03-31 02:28:47 +0900

    added copy code for RC2

---
+++ cprc2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# simple script to setup with RC2
+ 
+
+# copy Makefile and Makefile.config to ../caffe-rc2
+cp Makefile ../caffe-rc2/
+cp Makefile.config ../caffe-rc2/
+
+cp -r external ../caffe-rc2/
+
